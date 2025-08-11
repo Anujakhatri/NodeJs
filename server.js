ea6e8a3f1833bf9fc53f9173cad46bd120244d8b
Anuja khatri <70836354+Anujakhatri@users.noreply.github.com>
2025-08-11 19:10:47 +0545

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http= require('http');
 
 http.createServer((req,res)=>{
-    res.write("This is anuja");
-res.end("Heloo Anuja! This is my first server");
+    res.write("This is Anuja");
+res.end("Heloo Server ! This is my first server");
 }).listen(4800);
